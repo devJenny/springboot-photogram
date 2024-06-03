b1a29272cc9b967ce1f2faf57be61fc159c04585
devjenny <devje@naver.com>
2024-06-03 22:21:06 +0900

    Feat: `User` entity 추가

---
+++ src/main/java/com/example/photogram/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.example.photogram.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    private String name;
+    private String website;
+    private String bio; // 자기소개
+    private String email;
+    private String phone;
+    private String gender;
+
+    private String profileImageUrl; // 사진
+    private String role; // 권한
+
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    public void createdDate() {
+        this.createdDate = LocalDateTime.now();
+    }
+}
