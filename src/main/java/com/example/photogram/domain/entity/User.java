85f258cefa0d03528f4e3762cb11d4b7e6b8f139
devjenny <devje@naver.com>
2024-06-04 23:40:25 +0900

    Feat: 비밀번호 암호화 및 중복 아이디 가입 불가 처리

--- src/main/java/com/example/photogram/domain/entity/User.java
+++ src/main/java/com/example/photogram/domain/entity/User.java
@@ -13,11 +13,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String username;
     private String password;
 
--- src/main/java/com/example/photogram/service/AuthService.java
+++ src/main/java/com/example/photogram/service/AuthService.java
@@ -3,16 +3,24 @@ package com.example.photogram.service;
 import com.example.photogram.domain.entity.User;
 import com.example.photogram.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional // Write(Insert, Update, Delete)
     public User join(User user) {
         // 회원가입 진행
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole("ROLE_USER"); // 관리자 ROLE_ADMIN
         User userEntity = userRepository.save(user);
         return userEntity;
     }
