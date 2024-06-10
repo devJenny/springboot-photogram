c87016b6f0c4d7656d2494541694d8ef7a073176
devjenny <devje@naver.com>
2024-06-10 23:01:30 +0900

    Feat: 공통 응답 Script 추가

--- src/main/java/com/example/photogram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/example/photogram/handler/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.photogram.handler;
 
 import com.example.photogram.dto.CMResDto;
 import com.example.photogram.handler.ex.CustomValidationException;
+import com.example.photogram.util.Script;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,11 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public CMResDto<?> validException(CustomValidationException e) {
-        return new CMResDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+    public String validException(CustomValidationException e) {
+        // CMResDto, Script 비교
+        // 1. 클라이언트에게 응답할 때는 Script 좋음
+        // 2. Ajax 통신 - CMResDto
+        // 3. Android 통신 - CmResDto
+        return Script.back(e.getErrorMap().toString());
     }
 }
