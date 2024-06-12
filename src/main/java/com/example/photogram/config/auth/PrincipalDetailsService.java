c6b3c44fa9a534dc20320038e36ca5de210aa65a
devjenny <devje@naver.com>
2024-06-12 23:38:33 +0900

    Feat: 로그인 인증 처리

--- src/main/java/com/example/photogram/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/example/photogram/config/auth/PrincipalDetailsService.java
@@ -1,15 +1,32 @@
 package com.example.photogram.config.auth;
 
+import com.example.photogram.domain.entity.User;
+import com.example.photogram.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다
+    // 2. 리턴이 잘되면 자동으로 userDetails 타입을 세션으로 만든다.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("username = " + username);
-        return null;
+
+        User userEntity = userRepository.findByUsername(username);
+
+        if (userEntity == null) {
+            return null;
+
+        } else {
+            return new principalDetails(userEntity);
+
+        }
     }
 }
