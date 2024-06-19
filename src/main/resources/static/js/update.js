d95a541cdc81d684dfb44bc363a0bb05ef53075a
devjenny <devje@naver.com>
2024-06-19 21:11:09 +0900

    Chore: button에서 submit으로 변경 및 폼 태그 액션 중지

--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,14 +1,15 @@
 // (1) 회원정보 수정
-function update(userId) {
+function update(userId, event) {
+    event.preventDefault(); // 폼태그 액션 증지
     let data = $("#profileUpdate").serialize();
     console.log(data);
 
     $.ajax({
-        type:"put",
+        type: "put",
         url: `/api/user/${userId}`,
         data: data,
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
-        dataType:"json"
+        dataType: "json"
     }).done(res => {
         console.log("update 성공");
         location.href = `/user/${userId}`;
