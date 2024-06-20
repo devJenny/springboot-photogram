edce57f02cf2b6e5a28e63d76577381ecba34c9c
devjenny <devje@naver.com>
2024-06-20 23:16:38 +0900

    Feat: 회원정보수정 optional 처리

--- src/main/java/com/example/photogram/handler/ex/CustomValidationApiException.java
+++ src/main/java/com/example/photogram/handler/ex/CustomValidationApiException.java
@@ -4,10 +4,15 @@ import java.util.Map;
 
 public class CustomValidationApiException extends RuntimeException {
 
+
     // 객체를 구분할 때
     private static final long serialVersionUID = 1L;
 
     private Map<String, String> errorMap;
+
+    public CustomValidationApiException(String message) {
+        super(message);
+    }
 
     public CustomValidationApiException(String message, Map<String, String> errorMap) {
         super(message);
--- src/main/java/com/example/photogram/service/UserService.java
+++ src/main/java/com/example/photogram/service/UserService.java
@@ -2,10 +2,13 @@ package com.example.photogram.service;
 
 import com.example.photogram.domain.entity.User;
 import com.example.photogram.domain.repository.UserRepository;
+import com.example.photogram.handler.ex.CustomValidationApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 @Service
@@ -18,7 +21,9 @@ public class UserService {
     public User userUpdate(int id, User user) {
 
         // 1. 영속화
-        User userEntity = userRepository.findById(id).get(); // optional -> 1. 무조건 찾음 get() 2. 못찾음 예외처리 orElseThrow()
+        User userEntity = userRepository.findById(id).orElseThrow(() -> {
+            return new CustomValidationApiException("찾을 수 없는 id입니다.");
+        });
 
         // 2. 영속화된 오브젝트를 수정 - 더티체킹 (업데이트 완료)
         userEntity.setName(user.getName());
