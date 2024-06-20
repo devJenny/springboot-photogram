beae4dfa4220d61daa8f79a698d6b97a7bc7c0b8
devjenny <devje@naver.com>
2024-06-20 23:42:12 +0900

    Feat: subscribe entity, repository 생성

---
+++ src/main/java/com/example/photogram/domain/entity/Subscribe.java
@@ -0,0 +1,43 @@
+package com.example.photogram.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subscribe_uk",
+                        columnNames = {"fromUserId","toUserId"}
+                )
+        }
+)
+public class Subscribe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name="fromUserId")
+    @ManyToOne
+    private User fromUser;
+
+    @JoinColumn(name="toUserId")
+    @ManyToOne
+    private User toUser;
+
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    public void createdDate() {
+        this.createdDate = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/photogram/domain/repository/SubscribeRepository.java
@@ -0,0 +1,7 @@
+package com.example.photogram.domain.repository;
+
+import com.example.photogram.domain.entity.Subscribe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
+}
