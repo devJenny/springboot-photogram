38d1416c727576fde1fa2a56b91cacb73d1533c1
devjenny <devje@naver.com>
2024-06-22 20:41:05 +0900

    Chore: `SubscribeRepository` 오타 확인 및 괄호 추가

--- src/main/java/com/example/photogram/domain/repository/SubscribeRepository.java
+++ src/main/java/com/example/photogram/domain/repository/SubscribeRepository.java
@@ -2,14 +2,19 @@ package com.example.photogram.domain.repository;
 
 import com.example.photogram.domain.entity.Subscribe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 
-    @Query(value="INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now()", nativeQuery=true)
-    void mSubscribe(int fromUserId, int toUserId);
+    @Modifying
+    @Query(value="INSERT INTO subscribe(fromUserId, toUserId, createdDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery=true)
+    void mSubscribe(@Param("fromUserId") int fromUserId, @Param("toUserId") int toUserId);
 
+
+    @Modifying
     @Query(value="DELETE FROM subscribe WHERE fromUserId=:fromUserId AND toUserId=:toUserId", nativeQuery=true)
-    void mUnSubscribe(int fromUserId, int toUserId);
+    void mUnSubscribe(@Param("fromUserId") int fromUserId, @Param("toUserId") int toUserId);
 
 }
