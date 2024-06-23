77ce1b7e86e085837ac616a48337614bdca972af
devjenny <devje@naver.com>
2024-06-24 00:19:42 +0900

    Feat: 로컬 서버에 이미지 업로드 로직 구현

---
+++ src/main/java/com/example/photogram/service/ImageService.java
@@ -0,0 +1,41 @@
+package com.example.photogram.service;
+
+import com.example.photogram.config.auth.PrincipalDetails;
+import com.example.photogram.domain.repository.ImageRepository;
+import com.example.photogram.web.dto.Image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void upload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();// 1.jpg
+        log.info("imageFileName: {}", imageFileName);
+
+        Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+
+
+        // 통신, I/O -> 예외 발생
+        try {
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
